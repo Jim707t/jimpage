1da30f59d4c3e308994d83b8bb5de842790426e0
jim707t <jimgamerwinner@gmail.com>
2024-07-05 22:26:00 -0400

    Update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,22 +1,39 @@
-import React, { ReactNode, useEffect, useRef } from 'react';
+import React, { ReactNode, useEffect, useRef, useState } from 'react';
 import Navbar from './Navbar/Navbar';
 
 export default function Layout({ children }: { children: ReactNode }) {
   const audioRef = useRef<HTMLAudioElement>(null);
+  const [isPlaying, setIsPlaying] = useState<boolean>(true);
 
   useEffect(() => {
     const audio = audioRef.current;
     if (audio) {
       audio.volume = 0.3;  // Adjust volume as needed
-      audio.play().catch((error) => {
-        console.log('Autoplay prevented:', error);
-      });
+      if (isPlaying) {
+        audio.play().catch((error) => {
+          console.log('Autoplay prevented:', error);
+        });
+      } else {
+        audio.pause();
+      }
     }
-  }, []);
+  }, [isPlaying]);
+
+  const togglePlay = () => {
+    setIsPlaying(prevIsPlaying => !prevIsPlaying);
+  };
 
   return (
     <div className="relative min-h-screen">
       <Navbar />
+      <div className="absolute top-0 right-0 m-4">
+        <button
+          onClick={togglePlay}
+          className="px-4 py-2 bg-transparent text-gray-200 rounded-md border border-gray-800 hover:bg-gray-200 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-400"
+        >
+          {isPlaying ? 'Pause' : 'Play'}
+        </button>
+      </div>
       <audio ref={audioRef} loop>
         <source src="/assets/background-music.mp3" type="audio/mpeg" />
         Your browser does not support the audio element.
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,9 +9,9 @@ export default function Navbar() {
     <div className="container mx-auto p-10 rounded-md border-b-2 border-gray-300">
       <div className="flex flex-col md:flex-row items-center justify-center">
         <div className="order-2 md:order-1 md:mr-4 text-center md:text-left">
-          <h1 className="text-2xl sm:text-4xl font-bold mb-2 md:mb-0">I&aposm Jim Quincy Nemorin</h1>
+          <h1 className="text-2xl sm:text-4xl font-bold mb-2 md:mb-0">Jim Quincy Nemorin</h1>
           <p className="text-sm sm:text-base md:text-base lg:text-lg xl:text-lg mt-2 text-gray-300">
-            I work every day to become a better human being. I like to build and learn things that interest me. I&aposm mainly interested in technology topics, but I&aposm interested in much more than that. I hope that one day I will see myself as the human I want to be.
+            I work every day to become a better human being. I like to build and learn things that interest me. Mainly interested in technology topics, but interested in much more than that. I hope that one day I will see myself as the human I want to be.
           </p>
           <p className="mt-2 sm:mt-0">
             <span className="text-gray-100">Email me here -&gt;</span>{" "}
@@ -22,7 +22,7 @@ export default function Navbar() {
         </div>
 
         <div className="order-1 md:order-2 mb-4 md:mb-0 md:ml-4">
-          <div className="w-32 sm:w-48 md:w-64 lg:w-72 xl:w-80">
+          <div className="w-32 sm:w-48 md:w-64 lg:w-72 xl:w-72">
             <Image
               src={"/assets/pfr.jpg"}
               alt="Profile"
