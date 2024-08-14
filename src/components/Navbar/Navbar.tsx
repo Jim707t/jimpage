66b565788c1e9c89bbd57bf7ceab25d75f380333
jim707t <jimgamerwinner@gmail.com>
2024-08-13 22:14:32 -0400

    Added quote

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
         <div className="order-2 md:order-1 md:mr-4 text-center md:text-left">
           <h1 className="text-2xl sm:text-4xl font-bold mb-2 md:mb-0">Jim Quincy Nemorin</h1>
           <p className="text-sm sm:text-base md:text-base lg:text-lg xl:text-lg mt-2 text-gray-300">
-            I work every day to become a better human being. I like to build and learn things that interest me. Mainly interested in technology topics, but interested in much more than that. I hope that one day I will see myself as the human I want to be.
+            The greatest battles are fought within. Damn! So relatable.
           </p>
           <p className="mt-2 sm:mt-0">
             <span className="text-gray-100">Email -&gt;</span>{" "}
